3d418bfbe0b1a3fc5ccd6f62ceb6af64ca58cfe8
Evgeny Lyozin <evgenylyozindev@gmail.com>
2021-04-07 15:08:09 +0300

    Обход DOM

--- JS_In_Browsers.js
+++ JS_In_Browsers.js
@@ -1,4 +1,5 @@
 //JS in Browsers
+
 //Регистрация обработчиков событий
 
 window.onload = function(){
@@ -28,3 +29,67 @@ document.addEventListener("busy",(e)=>{
         hideSpinner();
     }
 });
+
+//DOM API
+
+//поиск элементов в документе
+let spinner = document.querySelector("#spinner"); //поиск и возврате первого элемента или null
+let titles = document.querySelectorAll("h1, h2, h3"); // Возврат объекта NodeList, похожего на массив
+
+//свойства выбранного элемента
+spinner.parentNode;// ссылается на родителя элемента
+spinner.children;// содержит дочерние элементы в объекте NodeList
+spinner.childElementCount;// количество дочерних элементов типа Element
+spinner.firstElementChild;// первый дочерний элемент
+spinner.lastElementChild;// последний дочерний элемент
+spinner.nextElementSibling;// следующий родственный элемент
+spinner.previousElementSibling;// предыдущий родственный элемент
+
+
+//Рекурсивный обход объекта e типа Document или Element
+// с вызовом функции для объекта e и каждого его потомка
+
+function traverse(e, f){
+    f(e);
+    for(let child of e.children){
+        traverse(child,f);
+    }
+}
+
+function traverse2(e, f){
+    f(e);
+    let child = e.firstElementChild;
+    while(child !== null){
+        traverse2(child, f);
+        child = child.nextElementSibling;
+    }
+}
+
+// Обход узлов (Объектов Element, Text и Comment)
+
+spinner.parentNode;
+spinner.childNodes;
+spinner.firstChild;
+spinner.lastChild;
+spinner.nextSibling;
+spinner.previousSibling;
+spinner.nodeType; // Узлы Document имеют значение - 9, Element - 1, Text - 3, Comment - 8
+spinner.nodeValue; // Текстовое содержимое узла Text или Comment
+spinner.nodeName; // Имя HTML-дескриптора объекта Element в верхнем регистре
+
+// Вернуть весь текст внутри элемента или документа
+// на практике получить текстовое содержимое элемента можно через
+// e.textContent
+
+function textContent(e){
+    let s = "";
+    for (let child = e.firstChild; child !== null; child = child.nextSibling){
+        let type = child.nodeType;
+        if(type === 3){
+            s+=child.nodeValue;
+        } else if(type === 1){
+            s+=textContent(child);
+        }
+    }
+    return s;
+}
